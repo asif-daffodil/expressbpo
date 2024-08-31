29cb1b09983944ff23c8db81d39f81b3cc688fce
Asif Abir <88622477+asif-daffodil@users.noreply.github.com>
2024-08-31 11:53:21 +0600

    1153am

--- resources/views/components/expbpo/home/clients.blade.php
+++ resources/views/components/expbpo/home/clients.blade.php
@@ -8,44 +8,58 @@
                 </div>
             </div>
             <div class="col-md-8 d-flex align-items-center wow fadeInRight">
-                KMK is an outsourced accounting service provider that works with clients across a diverse range of industries. Our team of experienced accounting professionals has a deep understanding of the unique financial challenges faced by businesses in various industries, and we tailor our services to meet the specific needs of each client. 
+                MyNexusBpo is an outsourced accounting service provider that works with clients across a diverse range
+                of industries. Our team of experienced accounting professionals has a deep understanding of the unique
+                financial challenges faced by businesses in various industries, and we tailor our services to meet the
+                specific needs of each client.
             </div>
         </div>
         <div class="row py-5 d-none d-lg-block">
             <div class="w-100 d-flex flex-wrap gap-5">
                 <div class="border-top py-3 wow fadeInLeft">
                     <ul class="nav flex-column clientList">
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Fund Accounting</a></li>
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Trucking & Logistic</a></li>
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Wholesale Distribution</a></li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Fund Accounting</a>
+                        </li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Trucking &
+                                Logistic</a></li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Wholesale
+                                Distribution</a></li>
                     </ul>
                 </div>
                 <div class="border-top py-3 wow fadeInLeft">
                     <ul class="nav flex-column clientList">
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Manufacturing</a></li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Manufacturing</a>
+                        </li>
                         <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Real Estate</a></li>
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Entertainment</a></li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Entertainment</a>
+                        </li>
                     </ul>
                 </div>
                 <div class="border-top py-3 wow fadeInDown">
                     <ul class="nav flex-column clientList">
                         <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">CPG</a></li>
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Pharmaceuticals</a></li>
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Information Technology</a></li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Pharmaceuticals</a>
+                        </li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Information
+                                Technology</a></li>
                     </ul>
                 </div>
                 <div class="border-top py-3 wow fadeInRight">
                     <ul class="nav flex-column clientList">
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">VC Backed Startups</a></li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">VC Backed
+                                Startups</a></li>
                         <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Insurance</a></li>
                         <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Factoring</a></li>
                     </ul>
                 </div>
                 <div class="border-top py-3 wow fadeInRight">
                     <ul class="nav flex-column clientList">
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Hotel and Restaurants</a></li>
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Wireless Retail</a></li>
-                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Not for Profit Organizations</a></li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Hotel and
+                                Restaurants</a></li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Wireless Retail</a>
+                        </li>
+                        <li class="nav-item"><a href="" class="nav-link ps-0 fs-5 text-dark">Not for Profit
+                                Organizations</a></li>
                     </ul>
                 </div>
             </div>
@@ -81,10 +95,11 @@
             <a class="col-md-4 text-decoration-none" href="">
                 <div class="text-center">
                     <figure>
-                        <img src="{{ asset('images/for-us-based-companies-using-netsuite-sap-erp-system.webp') }}" alt=""
-                            class="img-fluid shadow shadow-md">
+                        <img src="{{ asset('images/for-us-based-companies-using-netsuite-sap-erp-system.webp') }}"
+                            alt="" class="img-fluid shadow shadow-md">
                     </figure>
-                    <h3 class="text-uppercase text-blue mt-3 mb-0 h5 mb-5">For US-Based companies using NetSuite/SAP ERP system</h3>
+                    <h3 class="text-uppercase text-blue mt-3 mb-0 h5 mb-5">For US-Based companies using NetSuite/SAP ERP
+                        system</h3>
                 </div>
             </a>
             <a class="col-md-4 text-decoration-none" href="">
@@ -102,7 +117,8 @@
                         <img src="{{ asset('images/for-us-based-fundmanagers-and-vc-firms.jpg') }}" alt=""
                             class="img-fluid shadow shadow-md">
                     </figure>
-                    <h3 class="text-uppercase text-blue mt-3 mb-0 h5 mb-5">For US- Based Trucking& Logistics Company</h3>
+                    <h3 class="text-uppercase text-blue mt-3 mb-0 h5 mb-5">For US- Based Trucking& Logistics Company
+                    </h3>
                 </div>
             </a>
         </div>
@@ -118,7 +134,9 @@
                     <span class="text-secondary fs-4">01</span>
                     <h2 class="text-success my-3">Cost-Effective.</h2>
                     <p class="text-muted">
-                        We bring our extensive accounting & tax experience to the table, so that you can focus on your own business. You may leave the debits for us to handle, while you get the credits. We have highly experienced team and we call ourselves seasoned professionals.
+                        We bring our extensive accounting & tax experience to the table, so that you can focus on your
+                        own business. You may leave the debits for us to handle, while you get the credits. We have
+                        highly experienced team and we call ourselves seasoned professionals.
                     </p>
                 </div>
             </div>
@@ -127,7 +145,9 @@
                     <span class="text-secondary fs-4">02</span>
                     <h2 class="text-success my-3">Cost-Effective.</h2>
                     <p class="text-muted">
-                        It’s simple. You get the best quality services while you leverage cost efficiencies due to geographical differences. You do not incur costs for payroll taxes, health benefits, PTO and of course the costs for incorrect hiring and employee turnover.
+                        It’s simple. You get the best quality services while you leverage cost efficiencies due to
+                        geographical differences. You do not incur costs for payroll taxes, health benefits, PTO and of
+                        course the costs for incorrect hiring and employee turnover.
                     </p>
                 </div>
             </div>
@@ -136,7 +156,9 @@
                     <span class="text-secondary fs-4">03</span>
                     <h2 class="text-success my-3">Data Security.</h2>
                     <p class="text-muted">
-                        We are an ISO/IEC 27001:2013 certified company and we follow the best practices for the data security meticulously. We heavily use technology and automation is our passion. We take pride in calling ourselves a software agnostic company.
+                        We are an ISO/IEC 27001:2013 certified company and we follow the best practices for the data
+                        security meticulously. We heavily use technology and automation is our passion. We take pride in
+                        calling ourselves a software agnostic company.
                     </p>
                 </div>
             </div>
