54c49ce48f5b576d97a08b742569ebb07f6ae825
asif-daffodil <abir@dipti.com.bd>
2024-09-02 18:10:35 +0600

    922024610pm

---
+++ resources/views/pages/company-overview.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.express')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 px-0">
+            <img src="{{asset('images/office.jpg')}}" alt="" class="img-fluid w-100" style="height: 50vh; object-fit: cover">
+        </div>
+    </div>
+</div>
+<div class="container my-4">
+    <div class="row">
+        <div class="col-md-4">
+            <h5>Our Company Overview</h5>
+            <h1>Excellence through integrity</h1>
+        </div>
+        <div class="col-md-8">
+            MyNexusBPO is a leading outsourced accounting, and tax service provider. To address the growing outsourcing needs of small & medium as well as mid-market businesses, we provide end-to-end tax and accounting services as a one stop solution for our clients. With years of experience, knowledge, and integrity, we become reliable extensions of our clients’ offices, so they can focus on their core competencies. Our team of 475+ professionals including certified public accountants, chartered accountants, and staff accountants make this possible.
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompanyOverview;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/company-overview', [CompanyOverview::class, 'index'])->name('company-overview');
 
 require __DIR__.'/auth.php';
